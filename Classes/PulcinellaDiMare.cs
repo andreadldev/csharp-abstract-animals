aed59341a2563f8f88bb98fac0c3249814711612
andreadldev <andrea.delisi2@gmail.com>
2023-03-23 16:24:21 +0100

    Aggiunto pulcinella

---
+++ Classes/PulcinellaDiMare.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals.Classes
+{
+    public class PulcinellaDiMare : Animale, INuotante, IVolante
+    {
+        public void Nuota()
+        {
+            Console.WriteLine("Sto nuotando!!!");
+        }
+        public void Vola()
+        {
+            Console.WriteLine("Sto volando!!!");
+        }
+        public override void Mangia()
+        {
+            Console.WriteLine("Pesci");
+        }
+
+        public override void Verso()
+        {
+            Console.WriteLine("...");
+        }
+    }
+}
