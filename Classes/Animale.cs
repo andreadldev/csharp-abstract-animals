13a2212ca5b3b010c193152a9d07c7ba54ed8db2
andreadldev <andrea.delisi2@gmail.com>
2023-03-23 14:41:54 +0100

    Classe astratta e sottoclassi

---
+++ Classes/Animale.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public abstract class Animale
+    {
+        public void Dormi()
+        {
+            Console.WriteLine("Zzz");
+        }
+
+        public abstract void Verso();
+
+        public abstract void Mangia();
+    }
+
---
+++ Classes/Passerotto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals.Classes
+{
+    public class Passerotto : Animale
+    {
+        public override void Mangia()
+        {
+            Console.WriteLine("Bruchi");
+        }
+
+        public override void Verso()
+        {
+            Console.WriteLine("Cinguetta");
+        }
+    }
+}
